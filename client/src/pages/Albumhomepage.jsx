41c12a81c371b3a553d5f29a9c8dfad1900cbeae
caique <caiqueestrela538@gmail.com>
2025-10-24 01:33:37 -0300

    Fetch user albums with react-query
    Replaces mock album data in Albumhomepage.jsx with real data fetched using react-query and a new getUserAlmbums API utility. Adds loading and error states for improved user experience.

--- client/src/pages/Albumhomepage.jsx
+++ client/src/pages/Albumhomepage.jsx
@@ -3,10 +3,24 @@ import { Link } from "react-router";
 import UserNavbar from "../components/UserNavbar";
 import AlbumCreateModal from "../components/AlbumCreateModal";
 import styles from "./../styles/pages/Albumhomepage.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { getUserAlmbums } from "../utils/apiUserAlbums";
 
 function Albumhomepage() {
     const [showModal, setShowModal] = useState(false);
 
+    const userId = 1;
+
+    const {
+        data: albums,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ["albums", userId],
+        queryFn: () => getUserAlmbums(userId),
+    });
+
+    if (error) return <p>Erro: {error.message}</p>;
     return (
         <>
             <header>
@@ -36,18 +50,21 @@ function Albumhomepage() {
                 </div>
 
                 <div className={styles.photos_container}>
-                    {/* mock de álbuns */}
-                    {[...Array(8)].map((_, i) => (
-                        <Link
-                            key={i}
-                            to="/album/1"
-                            className={styles.photo_box}
-                        >
-                            <img src="/images/cover.jpg" alt="" />
-                            <h3>family vacation 2025</h3>
-                            <p>2 photos</p>
-                        </Link>
-                    ))}
+                    {isLoading ? (
+                        <p>Carregando álbuns...</p>
+                    ) : (
+                        albums.map((album) => (
+                            <Link
+                                key={album.id}
+                                to={`album/${album.id}`}
+                                className={styles.photo_box}
+                            >
+                                <img src="/images/cover.jpg" alt="" />
+                                <h3>{album.title}</h3>
+                                <p>{album.description}</p>
+                            </Link>
+                        ))
+                    )}
                 </div>
             </main>
 
---
+++ client/src/utils/apiUserAlbums.js
@@ -0,0 +1,9 @@
+export async function getUserAlmbums(userId) {
+    try {
+        const response = await fetch(`http://localhost:3001/albums/${userId}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
